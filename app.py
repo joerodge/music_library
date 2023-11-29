c8f3174f91ac0f1f8e35f6a1f228001cdd27476c
Joe Rodgers <joerodgers89@outlook.com>
2023-11-29 16:51:01 +0000

    Added Application class for app.py main program

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
-from lib.database_connection import DatabaseConnection
-from lib.artist_repository import ArtistRepository
-from lib.album import Album
+
 from lib.album_repository import AlbumRepository
+from lib.artist_repository import ArtistRepository
+from lib.database_connection import DatabaseConnection
+
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
+
+    def get_all_artists(self):
+        artist_repository = ArtistRepository(self._connection)
+        artists = artist_repository.all()
+        return [(artist.id, artist.name) for artist in artists]
+
+    def get_all_albums(self):
+        album_repository = AlbumRepository(self._connection)
+        albums = album_repository.all()
+        return [(album.id, album.title) for album in albums]
+
+    def run(self):
+        print("What would you like to do?\n  1 - List all albums\n  2 - List all artists\n")
+        choice = input('Enter you choice: ').strip()
+        if choice == '1':
+            data, choice = self.get_all_albums(), 'albums'
+        elif choice == '2':
+            data, choice = self.get_all_artists(), 'artists'
+        else:
+            print("\nInvalid choice\n")
+            return False
+        
+        print(f"\nHere is the list of {choice}:")
+        print('  * ' + '\n  * '.join(f"{entry[0]} - {entry[1]}" for entry in data))
+        return True
 
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
-
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
-
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
-
-print()
-
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
-for album in albums:
-    print(album)
-
-print()
-
-print(album_repository.find(1))
+if __name__ == '__main__':
+    app = Application()
+    while not app.run():
+        app.run()
